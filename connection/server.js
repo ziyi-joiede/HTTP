b23d1b30dc63b53d31f8d9579fe0f1ff9fa812e0
YoNgWeI <30373219+YoNgWeI1992@users.noreply.github.com>
2021-07-17 15:45:54 +0800

    添加 connection

---
+++ connection/server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+
+	const html = fs.readFileSync('test.html', 'utf8');
+	const img = fs.readFileSync('test.jpg');
+
+	// connetion 每个请求都会用一个 tcp 连接
+	if (req.url === '/') {
+		res.writeHead(200, {
+			'Content-Type': 'text/html',
+			'Connection': 'close'
+		});
+		res.end(html);
+	} else {
+		res.writeHead(200, {
+			'Content-Type': 'image/jpg',
+			'Connection': 'close'
+		});
+		res.end(img);
+	}
+
+});
+
+
+
+server.listen(3000, () => {
+	console.log('server is running');
+})
