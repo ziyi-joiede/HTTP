983c36d41f119ee6d16bdff5c3e5e93df07a9fe4
YoNgWeI <30373219+YoNgWeI1992@users.noreply.github.com>
2021-07-17 15:21:26 +0800

    继续 cookie 学习

--- cookie/server.js
+++ cookie/server.js
@@ -14,17 +14,24 @@ const server = http.createServer((req, res) => {
 		// });
 
 		// 设置多个 cookie,
+
 		// max-age: cookie 的过期时间
 		// httpOnly: 禁止 javascript 访问 cookie
+		// res.writeHead(200, {
+		// 	'Content-Type': 'text/html',
+		// 	'Set-Cookie': ['id=123;max-age=2', 'abc=456;httpOnly']
+		// });
+
 		// domain: coookie有访问域权限的设定的，一般当前域写了 cookie，其他域不能访问
 		// 例如：	在 a.com 网站下写一个 cookie，此cookie只能在 a.com 下被访问，在 b.com 是不能访问的
-		//  通过设置可以使同一个域名下的二级域名test.a.comn能够访问 a.com 的 cookie
-
-		res.writeHead(200, {
-			'Content-Type': 'text/html',
-			'Set-Cookie': ['id=123;max-age=2', 'abc=456;httpOnly']
-		});
-
+		// 通过设置可以使同一个域名下的二级域名test.a.comn能够访问 a.com 的 cookie
+		const host = req.headers.host;
+		if (host === 'test.com') {
+			res.writeHead(200, {
+				'Content-Type': 'text/html',
+				'Set-Cookie': ['id=123;max-age=2', 'abc=456;httpOnly;domain=test.com']
+			});
+		}
 		res.end(html);
 	}
 
